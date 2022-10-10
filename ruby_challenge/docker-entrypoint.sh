97a6d2e643511fa9910c52b1f21ecd3f10ec7b6b
David <david.marie@buyco.co>
2022-10-10 11:06:55 +0200

    fix docker volume

--- ruby_challenge/docker-entrypoint.sh
+++ ruby_challenge/docker-entrypoint.sh
@@ -8,11 +8,11 @@ then
     fi
     bundle check || bundle install
     bin/rails db:migrate 2>/dev/null || bin/rails db:prepare
-    
+
     if [[ "$RAILS_ENV" == 'production' ]]; then
         echo 'Start production server'
     fi
-    
+
     bundle exec rails s -b 0.0.0.0
 elif [ "$1" = 'worker' ]
 then
