1cb94ef4bfcd9bec1d2d8120520f2553f383f726
rubyistdotjs <rubyistdotjs@gmail.com>
2022-10-14 10:07:09 +0200

    Remove infinite echo in docker-entrypoint

--- ruby_challenge/docker-entrypoint.sh
+++ ruby_challenge/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'api' ]
-then
+if [ "$1" = 'api' ]; then
     if [ -f tmp/pids/server.pid ]; then
         rm tmp/pids/server.pid
     fi
@@ -14,15 +13,13 @@ then
     fi
 
     bundle exec rails s -b 0.0.0.0
-elif [ "$1" = 'worker' ]
-then
+elif [ "$1" = 'worker' ]; then
     bundle check || bundle install
+
     echo "Start worker"
-    while true
-    do
-        echo "Add worker stating command here"
-        sleep 10
-    done
+    echo "Add worker starting command here"
+
+    sleep infinity
 else
     echo "Invalid argument send to docker-entrypoint.sh"
     exit 0
